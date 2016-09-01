dc7216ce10486c5346ed63d70f95d1499648b487
Ysabel Bombardiere <ysabelestradab@gmail.com>
2016-09-01 14:07:23 -0400

    created 0-100 array

--- array.rb
+++ array.rb
@@ -1,5 +1,31 @@
-mined_minds = [1, 2, 3, 4, 5]
 
-mined_minds.each do |mined_minds|
-	puts mined_minds
+
+# mined_minds = [1, 2, 3, 4, 5]
+
+# mined_minds.each do |mined_minds|
+# 	puts mined_minds
+# end
+
+# puts array_hundred = Array.new(100){ |i| i + 1 }
+
+# array_hundred = []
+
+# for (i=1, i+1, i<=100)
+
+#  	array_hundred = Array.push(i)
+#  	puts array_hundred
+
+# end
+
+# create Array that is 3 in length and the elements are related to the array index
+# puts Array.new(3){ |index| index ** 2 }
+
+#return 0,1,4 index [0,1,2]
+
+#populate array from 1 to 100
+array_hundred = Array.new(100){ |i| i + 1 }
+
+#loop through each element of array and print it to the terminal
+array_hundred.each do |array_hundred|
+	puts array_hundred
 end
